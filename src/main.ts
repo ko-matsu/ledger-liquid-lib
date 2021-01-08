6d1ef424c650c0104bb82fa3c6f44658d15f4437
k-matsuzawa <tangrambs803@gmail.com>
2021-01-09 01:53:15 +0900

    fix: disconnect

--- src/main.ts
+++ src/main.ts
@@ -130,8 +130,8 @@ function createWindow() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     // mainWindow = null;
-    const ret = await Ledger.disconnect();
-    console.log('disconnect:', ret);
+    await Ledger.disconnect();
+    console.log('disconnect');
   });
 
   // ~~~ BASIC LEDGER EXAMPLE ~~~
@@ -158,10 +158,12 @@ function createWindow() {
 app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function() {
+app.on("window-all-closed", async function() {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
+    await Ledger.disconnect();
+    console.log('disconnect');
     app.quit();
   }
 });
